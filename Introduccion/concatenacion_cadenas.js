5c45d28c3a75a8142d4085bfbba3e723f26eb130
FrankAldair <ald4gurtoruizfrank@gmail.com>
2023-02-24 12:50:59 -0500

    Capitulo1

---
+++ Introduccion/concatenacion_cadenas.js
@@ -0,0 +1,9 @@
+var nombre = "Frank";
+var apellido = "Agurto";
+var  nombreCompleto = nombre + ' ' + apellido; //concatetanacion
+console.log(nombreCompleto);
+
+var nombreCompleto2 = 'Frank' + ' ' + 'Aldair';
+console.log(nombreCompleto2);
+ var x = nombre + 19;
+ console.log(x);
--- Introduccion/tipos_datos.js
+++ Introduccion/tipos_datos.js
@@ -1,50 +1,48 @@
 /*Tipos de datos en java script*/
+
 //Strings: Cadena de texto
 var nombre = "Carlos";   //Creamos la variable y le asignamos el valor a dicha variable
 console.log(nombre);    //Imprimimos en consola dicha variable
 console.log (typeof nombre);    //Imprimimos en consola el tipo de dato con el metodo "typeof"
+
 //Number: valor numerico
 var numero = 100;
 console.log(typeof numero);
- //Object: Estructura de datos flexibles que permiten almacenar y manipular informacion
+ 
+//Object: Estructura de datos flexibles que permiten almacenar y manipular informacion
  var objet = {
     nombre: "Frank",
     edad: 19,
     telefono: "987654321"
  }
  console.log(typeof objet);
-//Undefined: No tiene un valor asignado
+
+ //Undefined: No tiene un valor asignado
 var x;
 console.log(typeof x);
+
 //Boolean: True False
 var verdadero = true;
 console.log (typeof verdadero);
-/*//Symbol
-const emojiSymbols = {
-  thumbsUp: Symbol('👍'),
-  heart: Symbol('❤️'),
-  smiley: Symbol('😊'),
-};
 
-const message = '¡Hola! Me encantó el nuevo diseño de la aplicación 👍❤️😊';
-
-const emojiCounts = {
-  [emojiSymbols.thumbsUp]: 1,
-  [emojiSymbols.heart]: 1,
-  [emojiSymbols.smiley]: 1,
-};
-
-for (let char of message) {
-  if (char in emojiSymbols) {
-    emojiCounts[emojiSymbols[char]]++;
-  }
-}
-
-console.log(emojiCounts);*/
 //Function
-function sumar (num1, num2){
-    return num1 + num2;     //Llamando a la funcion y asignando el resultado a una variable
-}
-let resultado = sumar (3,5);
-console.log(resultado)
-//Comentario prueba git
+function myfunction(){}
+console.log(typeof myfunction);
+
+//Tipo de clase es una funcion: Los tipos de clase son conciderados como funciones
+class persona{
+  constructor(nombre, apellido){
+    this.nombre = Frank;
+    this.apellido = Agurto;
+    }
+  }
+console.log(typeof persona);
+
+//Array
+var autos = ['BMW','Audi','Volvo']
+console.log(typeof autos);
+
+//empty string : un dato vacio
+var z = '';
+console.log(z);
+console.log(typeof z)
