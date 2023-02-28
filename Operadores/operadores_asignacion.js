47f8a61314aafc218472d07382f4757f26b25e66
brayan_dev <brayan@sgesystems.com>
2023-02-28 09:38:34 -0500

    cambios

---
+++ Operadores/operadores_asignacion.js
@@ -0,0 +1,23 @@
+//Asignacion =
+x = y   //x = y
+//Asignacion de adicion +=
+x += y  //x = x+y
+//Asignacion de resta   -=
+x -= y //x = x-y
+//Asignacion de multiplicacion  *=
+x *= y //x = x*y
+//Asignacion de division    /=
+x /= y //x= x/y
+//Asignacion de residuo %=
+x %= y //x = x%y
+//Asignacion de exponenciacion  **=
+x **= y //x = x**y
+//Asignacion de desplazamiento a la izquiera
+//Asignacion de desplazamiento a la derecha
+//Asignacion de desplazamiento a la derecha sin signo
+//Asignacion AND bit a bit
+//Asignacion XOR bit a bit
+//Asignacion OR bit a bit
+//Asignacion AND logico
+//Asignacion OR logico
+//Asignacion de asignacion logica
