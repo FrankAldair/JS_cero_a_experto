a623f5dd069d43ad86c9805dd8bd5cc829d7ffc3
brayan_dev <brayan@sgesystems.com>
2023-03-07 10:42:16 -0500

    cambs

--- Operadores/07_funcion_isNan.js
+++ Operadores/07_funcion_isNan.js
@@ -1,1 +1,25 @@
-isNaN("string");
+//isNaN(Not a Number):Devuelve true si el valor no es un numero; si el valor es un numero devuelve false
+isNaN("string");    //Devuelve true
+isNaN(123); //Devuelve false
+
+//Ejemplo:
+let miNumero = "18x";
+
+let edad = Number(miNumero);
+console.log(edad);
+if(isNaN(edad)){
+    console.log("No es un numero");
+}else{
+    if(edad >= 18){
+        console.log("Es mayor de edad, puede votar");
+    }
+    else{
+        console.log("Es menor de edad, no puede votar");
+    }
+}
+
+if(isNaN(edad)){
+    console.log("No es un numero");
+}else{
+    let resultado = (edad >= 18)? console.log("Eres mayor de edad") : console.log("Eres menor de edad");
+}
