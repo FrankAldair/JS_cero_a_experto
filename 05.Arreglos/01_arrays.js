9f61879c7053e16b0825654c07f7b7c9fd75b409
FrankAldair <ald4gurtoruizfrank@gmail.com>
2023-03-20 15:51:49 -0500

    cambioss

---
+++ 05.Arreglos/01_arrays.js
@@ -0,0 +1,5 @@
+//Un array siempre 
+//let autos = new Array('BMW','Mercedes Benz','Volvo'); //Let si puede cambiar sus valores
+const autos = ['BMW','Mercedes','Volvo'];   //Const siempre tiene un valor fijo
+console.log(autos);
+//ERES MUY BOT :)
